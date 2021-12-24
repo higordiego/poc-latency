36c8b852658faedb03cab007313cea475b97ae83
Higor Diego <higordiego@Higors-MacBook-Pro.local>
2021-12-24 09:12:32 -0300

    add: bitbucket integration

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/higordiego/poc-latency
+
+go 1.17
+
+require github.com/tcnksm/go-httpstat v0.2.0 // indirect
---
+++ sender.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+)
+
+func main() {
+	conf := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	conn, err := tls.Dial("tcp", "higordiego.com.br:443", conf)
+	if err != nil {
+		log.Println("Error in Dial", err)
+		return
+	}
+
+	defer conn.Close()
+	certs := conn.ConnectionState().PeerCertificates
+	for _, cert := range certs {
+		fmt.Printf("Issuer Name: %s\n", cert.Issuer)
+		fmt.Printf("Expiry: %s \n", cert.NotAfter.Format("2006-January-02"))
+		fmt.Printf("Common Name: %s \n", cert.Issuer.CommonName)
+
+	}
+}
